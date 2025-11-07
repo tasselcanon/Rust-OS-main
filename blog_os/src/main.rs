1ee3dc718e9f9e23e93872ae39eb6f85b912c154
tassel <3350486114@qq.com>
2025-11-07 23:09:53 +0800

    第二天，配置了工具链

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -7,7 +7,17 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+static HELLO: &[u8] = b"Tassel!";
+#[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &bybe) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = bybe;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
