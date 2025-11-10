3e3f4950f24d57a712a9e3af5d9d56f43af35db2
tassel <3350486114@qq.com>
2025-11-10 22:46:28 +0800

    完成了 test 模块的大部分

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -5,14 +5,31 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+mod serial;
+mod vga_buffer;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-mod vga_buffer;
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10, // 纯个人风格，可切换成任何数字
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -25,9 +42,25 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial_assertion... ");
+    assert_eq!(0, 1);
+    serial_println!("[ok]")
 }
