7d24bef9c5c5c7e5154e99381aa972b017383fc3
tasselcanon <tasselcanon@gmail.com>
2025-11-13 22:22:12 +0800

    仅完善了一下，今天刷leetcode去了...

--- blog_os/tests/should_panic.rs
+++ blog_os/tests/should_panic.rs
@@ -15,19 +15,16 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[unsafe(no_mangle)]
+// 集成测试所有的代码全部放到 start 中执行
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test didn't panic]");
+    exit_qemu(QemuExitCode::Failed); // 万一都通过了就 Failed 
 
     loop {}
 }
 
-// 这个测试不能用我们通用的了，这个测试我们要定义我们自己的 test_runner 函数
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test(); // 做所有测试时，假如有测试 panic 了就执行 panic 函数了，所以返回 Success 了
-        serial_println!("[test didn't panic]");
-        exit_qemu(QemuExitCode::Failed); // 万一都通过了就 Failed 
-    }
-    exit_qemu(QemuExitCode::Success);
+fn should_fail() {
+    serial_println!("should failed... ");
+    assert_eq!(0, 1);
 }
